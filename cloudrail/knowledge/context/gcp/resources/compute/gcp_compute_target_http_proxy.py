7f17ce67bed52317a74074286b8adc99280e01f5
lirshindelman <lirshindelman@gmail.com>
2021-11-16 09:55:51 +0200

    CR-3454

---
+++ cloudrail/knowledge/context/gcp/resources/compute/gcp_compute_target_http_proxy.py
@@ -0,0 +1,39 @@
+from typing import List, Optional
+
+from cloudrail.knowledge.context.gcp.resources.constants.gcp_resource_type import GcpResourceType
+from cloudrail.knowledge.context.gcp.resources.gcp_resource import GcpResource
+
+
+class GcpComputeTargetHttpProxy(GcpResource):
+    """
+        Attributes:
+            name: (Required) A unique name of the resource.
+            url_map: A reference to the UrlMap resource that defines the mapping from URL to the BackendService.
+    """
+
+    def __init__(self,
+                 name: str,
+                 url_map: str):
+
+        super().__init__(GcpResourceType.GOOGLE_COMPUTE_TARGET_HTTP_PROXY)
+        self.name: str = name
+        self.url_map: url_map
+
+    def get_keys(self) -> List[str]:
+        return [self.name, self.project_id]
+
+    @property
+    def is_tagable(self) -> bool:
+        return False
+
+    def get_id(self) -> str:
+        return self.name
+
+    def get_cloud_resource_url(self) -> Optional[str]:
+        return f'{self._BASE_URL}/net-services/loadbalancing/advanced/targetHttpProxies/details/{self.name}?project={self.project}'
+
+    def get_type(self, is_plural: bool = False) -> str:
+        if not is_plural:
+            return 'Compute Target Http Proxy Details'
+        else:
+            return 'Compute Target Http Proxy Details'
