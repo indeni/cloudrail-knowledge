59123ecfd9038149cc52d75f6db26c2003ad5502
lirshindelman <lirshindelman@gmail.com>
2022-01-03 11:10:30 +0200

    CR-2840-AWS_CFN-load_balancer_listeners

--- tests/knowledge/context/aws/test_load_balancer_listener.py
+++ tests/knowledge/context/aws/test_load_balancer_listener.py
@@ -1,23 +1,12 @@
 from cloudrail.knowledge.context.aws.aws_environment_context import AwsEnvironmentContext
 from tests.knowledge.context.aws_context_test import AwsContextTest
-from tests.knowledge.context.test_context_annotation import context
+from tests.knowledge.context.test_context_annotation import context, TestOptions
 
 
 class TestLoadBalancerListener(AwsContextTest):
 
     def get_component(self):
         return 'load_balancer_listeners'
-
-    @context(module_path="https")
-    def test_https_listener(self, ctx: AwsEnvironmentContext):
-        distribution = ctx.load_balancer_listeners[0]
-        self.assertEqual(distribution.listener_protocol, 'HTTPS')
-        self.assertEqual(distribution.listener_port, 443)
-        self.assertEqual(distribution.default_action_type, 'forward')
-        self.assertFalse(distribution.redirect_action_protocol)
-        self.assertFalse(distribution.redirect_action_port)
-        self.assertEqual(distribution.get_cloud_resource_url(),
-                         'https://console.aws.amazon.com/ec2/v2/home?region=us-east-1#LoadBalancers:type=application')
 
     @context(module_path="http")
     def test_http_listener(self, ctx: AwsEnvironmentContext):
@@ -27,6 +16,17 @@ class TestLoadBalancerListener(AwsContextTest):
         self.assertEqual(distribution.default_action_type, 'forward')
         self.assertFalse(distribution.redirect_action_protocol)
         self.assertFalse(distribution.redirect_action_port)
+
+    @context(module_path="https", test_options=TestOptions(run_terraform=False, run_cloudmapper=False))
+    def test_https_listener(self, ctx: AwsEnvironmentContext):
+        distribution = ctx.load_balancer_listeners[0]
+        self.assertEqual(distribution.listener_protocol, 'HTTPS')
+        self.assertEqual(distribution.listener_port, 443)
+        self.assertEqual(distribution.default_action_type, 'forward')
+        self.assertFalse(distribution.redirect_action_protocol)
+        self.assertFalse(distribution.redirect_action_port)
+        self.assertEqual(distribution.get_cloud_resource_url(),
+                         'https://console.aws.amazon.com/ec2/v2/home?region=us-east-1#LoadBalancers:type=application')
 
     @context(module_path="http_redirect_type")
     def test_http_listener_with_redirect(self, ctx: AwsEnvironmentContext):
