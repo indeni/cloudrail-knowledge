ec186cd8818e7e38653174cb33665b70761c2b0e
Imanuelm <imanuel.m@indeni.com>
2022-01-10 13:44:44 +0200

    CR-2764: added fields for gke

--- cloudrail/knowledge/context/gcp/resources_builders/terraform/container_cluster_builder.py
+++ cloudrail/knowledge/context/gcp/resources_builders/terraform/container_cluster_builder.py
@@ -1,7 +1,8 @@
-from cloudrail.knowledge.context.gcp.resources.cluster.gcp_container_cluster import GcpContainerCluster, GcpContainerMasterAuthNetConfigCidrBlk, GcpContainerMasterAuthNetConfig, \
-    GcpContainerClusterAuthGrpConfig
+from cloudrail.knowledge.context.gcp.resources.cluster.gcp_container_cluster import GcpContainerCluster, GcpContainerMasterAuthNetConfigCidrBlk,\
+    GcpContainerMasterAuthNetConfig, GcpContainerClusterAuthGrpConfig, GcpContainerClusterNetworkConfig, GcpContainerClusterNetworkConfigProvider
 from cloudrail.knowledge.context.gcp.resources.constants.gcp_resource_type import GcpResourceType
 from cloudrail.knowledge.context.gcp.resources_builders.terraform.base_gcp_terraform_builder import BaseGcpTerraformBuilder
+from cloudrail.knowledge.utils.enum_utils import enum_implementation
 
 
 class ContainerClusterBuilder(BaseGcpTerraformBuilder):
@@ -16,7 +17,15 @@ class ContainerClusterBuilder(BaseGcpTerraformBuilder):
         authenticator_groups_config_block = self._get_known_value(attributes, "authenticator_groups_config")
         authenticator_groups_config = GcpContainerClusterAuthGrpConfig(authenticator_groups_config_block[0]["security_group"]) if authenticator_groups_config_block else None
 
-        container_cluster = GcpContainerCluster(name, location, cluster_ipv4_cidr, enable_shielded_nodes, master_authorized_networks_config, authenticator_groups_config)
+        ## Network Config
+        network_config = GcpContainerClusterNetworkConfig(GcpContainerClusterNetworkConfigProvider.PROVIDER_UNSPECIFIED, False)
+        if network_config_data := self._get_known_value(attributes, 'network_policy'):
+            network_config = GcpContainerClusterNetworkConfig(provider=enum_implementation(GcpContainerClusterNetworkConfigProvider,
+                                                                                           self._get_known_value(network_config_data[0], 'provider'),
+                                                                                           GcpContainerClusterNetworkConfigProvider.PROVIDER_UNSPECIFIED),
+                                                              enabled=self._get_known_value(network_config_data[0], 'enabled', False))
+
+        container_cluster = GcpContainerCluster(name, location, cluster_ipv4_cidr, enable_shielded_nodes, master_authorized_networks_config, authenticator_groups_config, network_config)
         container_cluster.labels = self._get_known_value(attributes, "resource_labels")
 
         return container_cluster
@@ -27,6 +36,7 @@ class ContainerClusterBuilder(BaseGcpTerraformBuilder):
     @staticmethod
     def build_master_authorized_networks_config(master_authorized_networks_config: dict) -> GcpContainerMasterAuthNetConfig:
         cidr_blocks_list = master_authorized_networks_config.get("cidr_blocks", [])
-        cidr_blocks = [GcpContainerMasterAuthNetConfigCidrBlk(cidr_block.get("cidr_block"), cidr_block.get("display_name")) for cidr_block in cidr_blocks_list]
+        cidr_blocks = [GcpContainerMasterAuthNetConfigCidrBlk(cidr_block.get("cidr_block"), cidr_block.get("display_name")
+                                                              if cidr_block.get("display_name") else None) for cidr_block in cidr_blocks_list]
 
         return GcpContainerMasterAuthNetConfig(cidr_blocks)
